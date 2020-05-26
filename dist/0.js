dce9b10cd683edb74bf8f85826c9152551c54268
zhangyanling77 <18783226594@163.com>
2020-05-26 12:22:27 +0800

    update: restyle

--- dist/0.js
+++ dist/0.js
@@ -1,20 +1,8 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
-
-/***/ "./src/foo.js":
-/*!********************!*\
-  !*** ./src/foo.js ***!
-  \********************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-
-/* harmony default export */ __webpack_exports__["default"] = ('foo');
-
-// module.exports = 'foo';
-
-
-/***/ })
-
+  "./src/foo.js":
+  (function(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+    __webpack_require__.r(__webpack_exports__);
+    __webpack_exports__["default"] = ('foo');
+  })
 }]);
