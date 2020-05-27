d4214db21d367a92837a458e88585553e81401cd
zhangyanling77 <18783226594@163.com>
2020-05-27 18:59:46 +0800

    feat：chunk

--- dist/foo.bundle.js
+++ dist/foo.bundle.js
@@ -1,8 +1,13 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["foo"],{
+
   "./src/foo.js":
   (function(module, __webpack_exports__, __webpack_require__) {
+
     "use strict";
     __webpack_require__.r(__webpack_exports__);
+
     __webpack_exports__["default"] = ('foo');
+    
   })
+
 }]);
