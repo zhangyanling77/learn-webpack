89ea924c5ef9afeacc8a51b9aa3ee4eb319accee
zhangyanling77 <18783226594@163.com>
2020-06-02 16:52:21 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,34 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'), // 输出目录
     filename: '[name].bundle.js', // 输出文件名称
   },
-  module: {},
+  // 告诉webpack如何解析loader路径
+  resolveLoader: {
+    alias: {
+      'babel-loader': path.resolve(__dirname, 'loaders', 'babel-loader')
+    },
+    modules: [
+      path.resolve(__dirname, 'loaders'), "node_modules"
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          'loader1', 'loader2', 'loader3'
+          // {
+          //   loader: 'babel-loader',
+          //   options: {
+          //     presets: [
+          //       "@babel/preset-env",
+          //       "@babel/preset-react"
+          //     ]
+          //   }
+          // }
+        ]
+      }
+    ]
+  },
   plugins: [
     /**
      * 这个参数配置要删除哪些文件，和不要删除哪些文件，不要删除的文件前面加个逻辑运算符非 ! ，*号可以通过占位符来处理，表示什么开头，什么结尾
