04a89080e9292cd1463dbfc18ade3e40fbb4f219
zhangyanling <18783226594@163.com>
2020-06-12 21:18:34 +0800

    refactor: update

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   mode: 'development', // 标识不同的环境，development 开发 | production 生产
   devtool: 'none', // 不生成 sourcemap 文件
   // entry: './src/index.js', // 文件入口
-  entry: './ast/4.tree-shaking.js',
+  entry: './src/4.tree-shaking.js',
   output:  {
     path: path.resolve(__dirname, 'dist'), // 输出目录
     // filename: '[name].bundle.js', // 输出文件名称
