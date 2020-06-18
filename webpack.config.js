e66bc9ef60ebda0339aa4baa4ed87bf6487021f4
zhangyanling77 <18783226594@163.com>
2020-06-18 16:09:39 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -13,12 +13,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const DonePlugin = require('./plugins/DonePlugin');
+const AssetPlugin = require('./plugins/AssetPlugin');
+const AutoExternalPlugin = require('./plugins/AutoExternalPlugin');
+const ZipPlugin = require('./plugins/ZipPlugin');
 
 module.exports = {
   mode: 'development', // 标识不同的环境，development 开发 | production 生产
   devtool: 'none', // 不生成 sourcemap 文件
-  // entry: './src/index.js', // 文件入口
-  entry: './src/4.tree-shaking.js',
+  entry: './src/index.js', // 文件入口
+  // entry: './src/4.tree-shaking.js',
   output:  {
     path: path.resolve(__dirname, 'dist'), // 输出目录
     // filename: '[name].bundle.js', // 输出文件名称
@@ -26,12 +30,12 @@ module.exports = {
   },
   // 告诉webpack如何解析loader路径
   // resolveLoader: {
-  //   alias: {
-  //     'babel-loader': path.resolve(__dirname, 'loaders', 'babel-loader')
-  //   },
-  //   modules: [
-  //     path.resolve(__dirname, 'loaders'), "node_modules"
-  //   ]
+    // alias: {
+    //   'babel-loader': path.resolve(__dirname, 'loaders', 'babel-loader')
+    // },
+    // modules: [
+    //   path.resolve(__dirname, 'loaders'), "node_modules"
+    // ]
   // },
   module: {
     rules: [
@@ -41,7 +45,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             // 按需加载lodash 使用 babel-plugin-import.js 自己来实现一下
-            plugins: [['import', { library: 'lodash' }]]
+            // plugins: [['import', { library: 'lodash' }]]
           }
         }
         // use: [
@@ -66,9 +70,21 @@ module.exports = {
      * cleanOnceBeforeBuildPatterns: ['main*.*', '!vendor', '!vendor.manifest.json']
      */
     new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: ['**/*'] }),
+    // new ZipPlugin({
+    //   filename: 'assets.zip'
+    // }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: 'index.html'
+    }),
+    // new DonePlugin(),
+    // new AssetPlugin(),
+    new AutoExternalPlugin({
+      // jquery 模块名 jQuery是全局变量名 url是外链的CDN地址
+      jquery: {
+        expose: 'jQuery',
+        url: 'https://cdn.bootcss.com/jquery/3.1.0/jquery.js'
+      }
     })
   ],
   devServer: {}
